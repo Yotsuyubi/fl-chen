3187b23a4faf313f7c587471a3e50810f907f5ef
wehiroi <wehiroi@WihiroinoAir.lan>
2020-09-04 23:35:04 +0900

    windows build

--- build.sh
+++ build.sh
@@ -3,5 +3,10 @@
 cd view/
 npm run build
 cd ../
+
 cargo build --release
 sh osx_vst_bundler.sh FL-Chen target/release/libbasicvst.dylib
+mv FL-Chen.vst dest/FL-Chen.vst
+
+cargo build --target x86_64-pc-windows-gnu --release
+mv target/x86_64-pc-windows-gnu/release/basicvst.dll dest/FL-Chen.dll
